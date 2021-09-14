c5f225024dac459ad8a5621348a7a0821706e938
Bruce Zhou <bruce.zhou@zefr.com>
2021-09-14 16:41:55 -0700

    run examples with different settings

--- examples/tutorials/basic/your_first_hydra_app/6_composition/my_app.py
+++ examples/tutorials/basic/your_first_hydra_app/6_composition/my_app.py
@@ -6,7 +6,8 @@ import hydra
 
 @hydra.main(config_path="conf", config_name="config")
 def my_app(cfg: DictConfig) -> None:
-    print(OmegaConf.to_yaml(cfg))
+    # print(OmegaConf.to_yaml(cfg))
+    return None
 
 
 if __name__ == "__main__":
